7e71f25fee81ad6b6b6f8c3eac5231a2deb24dc5
Johann3421 <1355430@senati.pe>
2024-03-14 23:34:31 -0500

    Subiendo mi avance

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Loggin_2"
+include(":app")
+ 
